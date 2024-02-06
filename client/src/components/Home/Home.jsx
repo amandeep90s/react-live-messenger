96adafc811e63ae08d722a60d0fa0c5c554996ad
Amandeep Singh <programmeramandeep@gmail.com>
2024-02-06 20:33:10 +0530

    ChakraUI Friends List with Grid and Tabs

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,33 @@
+import { Grid, GridItem, Tabs } from "@chakra-ui/react";
+import { createContext, useMemo, useState } from "react";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+
+export const FriendContext = createContext();
+
 const Home = () => {
-  return <div></div>;
+  const [friendList, setFriendList] = useState([
+    { username: "aman", connected: true },
+    { username: "gaman", connected: false },
+  ]);
+
+  const contextValue = useMemo(
+    () => ({ friendList, setFriendList }),
+    [friendList]
+  );
+
+  return (
+    <FriendContext.Provider value={contextValue}>
+      <Grid templateColumns="repeat(10, 1fr)" h="100vh" as={Tabs}>
+        <GridItem colSpan={3} borderRight="1px solid gray">
+          <Sidebar />
+        </GridItem>
+        <GridItem colSpan={7}>
+          <Chat />
+        </GridItem>
+      </Grid>
+    </FriendContext.Provider>
+  );
 };
 
 export default Home;
--- client/src/components/Home/Sidebar.jsx
+++ client/src/components/Home/Sidebar.jsx
@@ -1,5 +1,44 @@
+import { ChatIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Circle,
+  Divider,
+  HStack,
+  Heading,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { Tab, TabList } from "@chakra-ui/tabs";
+import { useContext } from "react";
+import { FriendContext } from "./Home";
+
 const Sidebar = () => {
-  return <div></div>;
+  const { friendList } = useContext(FriendContext);
+
+  return (
+    <VStack py="1.4rem">
+      <HStack justify="space-evenly" w="100%">
+        <Heading size="md">Add Friend</Heading>
+        <Button>
+          <ChatIcon />
+        </Button>
+      </HStack>
+      <Divider />
+
+      <VStack as={TabList}>
+        {friendList.map((friend) => (
+          <HStack as={Tab} key={friend.username}>
+            <Circle
+              bg={friend.connected ? "green.500" : "red.500"}
+              w="20px"
+              h="20px"
+            />
+            <Text>{friend.username}</Text>
+          </HStack>
+        ))}
+      </VStack>
+    </VStack>
+  );
 };
 
 export default Sidebar;
